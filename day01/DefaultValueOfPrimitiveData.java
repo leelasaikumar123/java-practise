d5a2dab6a4c67e04ddb5b272f66702bd4e1e5fc8
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-05 14:38:02 +0530

    feat:added default values of primitive data type

---
+++ day01/DefaultValueOfPrimitiveData.java
@@ -0,0 +1,23 @@
+package day01;
+
+public class DefaultValueOfPrimitiveData {
+  static  int a;
+static short b;
+  static  long c;
+  static  byte d;
+  static  boolean e;
+  static  char f;
+  static float g;
+  static double h;
+  public static void main(String[] args) {
+    System.out.println("The Default Value Of Int is "+a);
+    System.out.println("The Default Value Of Short is "+b);
+    System.out.println("The Default Value Of Long is "+c);
+    System.out.println("The Default Value Of Byte is "+d);
+    System.out.println("The Default Value Of Boolean is "+e);
+    System.out.println("The Default Value Of Char is "+f);
+    System.out.println("The Default Value Of Float is "+g);
+    System.out.println("the Default Value Of Double is "+h);
+  }
+
+}
