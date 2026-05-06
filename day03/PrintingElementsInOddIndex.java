275f9bbf63788c583758a313712e4ef3830b3919
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-06 13:47:26 +0530

    feat:add program that prints elements in odd index

---
+++ day03/PrintingElementsInOddIndex.java
@@ -0,0 +1,17 @@
+package day03;
+
+import java.util.Scanner;
+
+public class PrintingElementsInOddIndex {
+        public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int[] array=new int[size];
+        for(int i=0;i<array.length;i++){
+            array[i]=sc.nextInt();
+        }
+    for(int i=1;i<array.length;i=i+2){
+    System.out.print(array[i]+" ");
+    }
+    }
+}
