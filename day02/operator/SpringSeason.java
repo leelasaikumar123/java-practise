5619acf0b779d4485dab05466a492a3d336b1b2c
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-07 15:57:02 +0530

    fix:program taking input from cla and telling is it spring or not

--- day02/operator/SpringSeason.java
+++ day02/operator/SpringSeason.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class SpringSeason {
    public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
-    int month=sc.nextInt();
-    int day=sc.nextInt();
+    int month=Integer.parseInt(args[0]);
+    int day=Integer.parseInt(args[1]);
     if ((month>=3 && day>=20) && (month<=6 && day <=20)){
         System.out.println("Its is a spring season");
     }
