b69e912a99bc64892236638b67702e16478508ba
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-07 14:46:34 +0530

    feat: add program that tells is it  a spring season or not

---
+++ day02/operator/SpringSeason.java
@@ -0,0 +1,17 @@
+package day02.operator;
+
+import java.util.Scanner;
+
+public class SpringSeason {
+   public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int month=sc.nextInt();
+    int day=sc.nextInt();
+    if ((month>=3 && day>=20) && (month<=6 && day <=20)){
+        System.out.println("Its is a spring season");
+    }
+    else{
+        System.out.println("Its Not a spring season");
+    }
+   } 
+}
