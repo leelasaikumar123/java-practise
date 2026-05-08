77c7526a6147ff0793c37146c14b4c12fc4e568e
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-08 16:17:31 +0530

    feat:added function that calculates the salary for an employee in a month

--- employee/EmployeeWages.java
+++ employee/EmployeeWages.java
@@ -3,7 +3,9 @@ package employee;
 import java.util.Random;
 
 public class EmployeeWages {
-   static Random random=new Random();
+  public static Random random=new Random();
+  public static int total_working_days=20;
+  public static int total_noof_working_hours=100;
     static{
         System.out.println("welcome to employee wages computation program");
     }
@@ -20,13 +22,61 @@ public class EmployeeWages {
         public static void wagesForFullTime(){
         System.out.println("The Monthly Salary Of An Full Time Employee is : "+(20*8*20));
     }
+    public static void wagesForFullTimeEmployee(){
+     int hours=0;
+     int days=0;
+     int salary=0;
+     while(hours<=total_noof_working_hours && days <= total_working_days){
+        int num=random.nextInt(2);
+        if(num==1){
+          days++;
+          hours=hours+8;
+          salary=salary+8*20;
+        }
+        else{
+            salary=salary+0;
+            days++;
+        }
+     }
+     System.out.println("The Salary for For This full time Emploee For This Month Is : "+salary);
+    }
+
+    public static void wagesForPartTimeEmployee(){
+     int hours=0;
+     int days=0;
+     int salary=0;
+     while(hours<=total_noof_working_hours && days <= total_working_days){
+        int num=random.nextInt(2);
+        if(num==1){
+          days++;
+          hours=hours+8;
+          salary=salary+8*10;
+        }
+        else{
+            salary=salary+0;
+            days++;
+        }
+     }
+     System.out.println("The Salary for For This Part Emploee For This Month Is : "+salary);
+    }
+
     public static void wagesForPartTime(){
-        System.out.println("The Monthly Salary Of An Part Time Employee is : "+(20*4*20));
+        System.out.println("The Monthly Salary Of An Part Time Employee is : "+(20*8*10));
     }
 
     public static void main(String[] args) {
         isEmplyeePresentOrNot();
-        wagesForFullTime();
-        wagesForPartTime();
+     int num=random.nextInt(2);
+     switch(num){
+        case 1:
+            wagesForFullTime();
+            wagesForFullTimeEmployee();
+             break;
+        default :
+              wagesForPartTime();
+              wagesForPartTimeEmployee();
+            break;    
+     }
     }
 }
+
