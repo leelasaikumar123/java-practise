f90c57a80853eae03272730b6229b83bf55e8da4
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-11 15:05:35 +0530

    feat:program that finds roots of an degree 2 Qudratic equation

---
+++ day05/Quadratic.java
@@ -0,0 +1,18 @@
+package day05;
+
+import java.util.Scanner;
+
+public class Quadratic {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int delta=(b*b)-4*a*c;
+        double root1=((b*-1)+Math.sqrt(delta))/(2*a);
+        double root2=((b*-1)-Math.sqrt(delta))/(2*a);
+       System.out.println("The Roots Are : ");
+       System.out.println(root1+" "+root2);
+        sc.close();
+    }
+}
