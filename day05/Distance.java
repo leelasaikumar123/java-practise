4b6ba077f528822ba53b44c72c1ef7e4fca57bea
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-11 14:52:48 +0530

    feat:program that calculates euclidean distance from origin to a point

---
+++ day05/Distance.java
@@ -0,0 +1,13 @@
+package day05;
+
+import java.util.Scanner;
+
+public class Distance {
+   public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int x=sc.nextInt();
+    int y=sc.nextInt();
+    double distance=Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
+    System.out.println("The Euclidean distance from the origin to the point is : "+distance);
+   } 
+}
