544f1cee2d8ff62b11b49c8c32f9e9b8a5008295
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-12 14:05:53 +0530

    feat:tells the time difference between start and end in stop watch

--- day06/StopWatch.java
+++ day06/StopWatch.java
@@ -1,14 +1,19 @@
 package day06;
-
 import java.util.Scanner;
 
 public class StopWatch {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int seconds=sc.nextInt();
-        int hours=seconds/3600;
-        int minutes=(seconds%3600)/60;
-        int second=seconds%60;
- System.out.println("Its Took " +hours +"hr "+minutes +"mins "+second+"sec ");
+       System.out.println("enter the start time in hr:min:sec");{
+        String startTime=sc.next();
+        System.out.println("enter the endtime time in hr:min:sec");
+        String endTime=sc.next();
+        String[] startTimeArray=startTime.split(":");
+        String[] endTimeArray=endTime.split(":");
+      int startTimeInSeconds=Integer.parseInt(startTimeArray[0])*3600+Integer.parseInt(startTimeArray[1])*60+Integer.parseInt(startTimeArray[2]);
+      int endTimeInSeconds=Integer.parseInt(endTimeArray[0])*3600+Integer.parseInt(endTimeArray[1])*60+Integer.parseInt(endTimeArray[2]);
+      int diffInSeconds=endTimeInSeconds-startTimeInSeconds;
+      System.out.println((diffInSeconds)/3600 +" hrs "+(diffInSeconds%3600)/60+" mins " +diffInSeconds%60 +" seconds");
+       }
     }
 }
