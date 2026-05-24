2c2e47dec1db9cf14b446a5289e0317d761f9403
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-24 12:06:26 +0530

    feat:[leela sai kumar meda] regular rexpression for first name

--- day19/UserRegistration.java
+++ day19/UserRegistration.java
@@ -5,6 +5,13 @@ import java.util.Scanner;
 public class UserRegistration {
     public static void main(String[] args) {
       Scanner sc=new Scanner(System.in);
-        
+    System.out.println("Enter First Name");
+    String firstName=sc.nextLine();
+    if(firstName.matches("[A-Z][a-z]{2,}")){
+    System.out.println("Its Following The Regular Expression");
+    }
+    else{
+        System.out.println("The first name should start with a capital letter and has min size of 3 characters");
+    }
     }
 }
