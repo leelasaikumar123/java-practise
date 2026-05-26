8bd92f614bda25edff2a59ea4bb421f4f9ef58dc
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-26 14:09:32 +0530

    feat:[leela sai kumar meda] we will give a sentence we should analyse and respond with happy or sad

--- day21/MoodAnalyserAlongWithExceptions/src/com/moodanalyser/MoodAnalyserClass.java
+++ day21/MoodAnalyserAlongWithExceptions/src/com/moodanalyser/MoodAnalyserClass.java
@@ -1,5 +1,17 @@
 package com.moodanalyser;
 
 public class MoodAnalyserClass {
+	public String analyseMood(String s) {
 
+	if(s.contains("sad")) {
+		return "SAD";
+	}
+	else {
+		return "HAPPY";
+	}
+
+
+
+	}	
 }
+
