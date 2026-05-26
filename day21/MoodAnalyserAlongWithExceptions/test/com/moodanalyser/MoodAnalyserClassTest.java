f1bc0fcc44ca9db12eee2200793fcfa0f9d4a407
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-26 12:13:25 +0530

    feat:[leela sai kumar meda] starting program

---
+++ day21/MoodAnalyserAlongWithExceptions/test/com/moodanalyser/MoodAnalyserClassTest.java
@@ -0,0 +1,14 @@
+package com.moodanalyser;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class MoodAnalyserClassTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
