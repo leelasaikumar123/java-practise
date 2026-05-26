aba7e1b46a06dc517a3e07f163ae3d5f992b7176
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-26 14:37:26 +0530

    refactor:[leela sai kumar meda] we created a parameterized and zero parameterized constructor

--- day21/MoodAnalyserAlongWithExceptions/src/com/moodanalyser/MoodAnalyserClass.java
+++ day21/MoodAnalyserAlongWithExceptions/src/com/moodanalyser/MoodAnalyserClass.java
@@ -1,17 +1,30 @@
 package com.moodanalyser;
 
 public class MoodAnalyserClass {
-	public String analyseMood(String s) {
+	String message;
 
+	public MoodAnalyserClass() {
+	}    
+	public MoodAnalyserClass(String message) {
+        this.message = message;
+    }
+
+    public String analyseMoodUsingInstanceVariable() {
+	if(message.contains("sad")) {
+		return "SAD";
+	}
+	else {
+		return "HAPPY";
+	}
+	}
+	
+	public String analyseMood(String s) {
 	if(s.contains("sad")) {
 		return "SAD";
 	}
 	else {
 		return "HAPPY";
 	}
-
-
-
 	}	
 }
 
