81f939f80ef98c0cc8d674cefa84b38962dfc440
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-29 10:25:51 +0530

    feat:[leela sai kumar meda] passing lambda as an parameter to another function

--- day23/MathOperationsProject/src/com/example/MathOperations.java
+++ day23/MathOperationsProject/src/com/example/MathOperations.java
@@ -4,22 +4,25 @@ public class MathOperations{
 
     public int add(int a, int b) {
         Calculation addition = (x, y) -> x + y;
-        return addition.calculation(a, b);
+        return Calculation.calculateResult(a,b,addition);
     }
         public int sub(int a, int b) {
-        Calculation addition = (x, y) -> x - y;
-        return addition.calculation(a, b);
+        Calculation subpraction = (x, y) -> x - y;
+        return Calculation.calculateResult(a,b,subpraction);
     }
          public int mul(int a, int b) {
-        Calculation addition = (x, y) -> x * y;
-        return addition.calculation(a, b);
+        Calculation multiplication = (x, y) -> x * y;
+       return Calculation.calculateResult(a,b,multiplication);
     }
         public int div(int a, int b) {
-        Calculation addition = (x, y) -> x / y;
-        return addition.calculation(a, b);
+        Calculation division = (x, y) -> x / y;
+        return Calculation.calculateResult(a,b,division);
     }
 
 }
 interface Calculation{
 public int calculation(int a,int b);
+public static int calculateResult(int a,int b,Calculation operation){
+    return operation.calculation(a, b);
+}
 }
