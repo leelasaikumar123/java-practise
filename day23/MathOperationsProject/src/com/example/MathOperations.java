ebb7c00b7cf966a4675960626b3d9e514fc7e8b6
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-29 10:18:25 +0530

    feat:performing math operations using lambda expressions

--- day23/MathOperationsProject/src/com/example/MathOperations.java
+++ day23/MathOperationsProject/src/com/example/MathOperations.java
@@ -1,5 +1,25 @@
 package com.example;
 
-public class MathOperations {
+public class MathOperations{
 
+    public int add(int a, int b) {
+        Calculation addition = (x, y) -> x + y;
+        return addition.calculation(a, b);
+    }
+        public int sub(int a, int b) {
+        Calculation addition = (x, y) -> x - y;
+        return addition.calculation(a, b);
+    }
+         public int mul(int a, int b) {
+        Calculation addition = (x, y) -> x * y;
+        return addition.calculation(a, b);
+    }
+        public int div(int a, int b) {
+        Calculation addition = (x, y) -> x / y;
+        return addition.calculation(a, b);
+    }
+
+}
+interface Calculation{
+public int calculation(int a,int b);
 }
--- day23/MathOperationsProject/test/com/example/MathOperationsTest.java
+++ day23/MathOperationsProject/test/com/example/MathOperationsTest.java
@@ -1,14 +1,30 @@
 package com.example;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 class MathOperationsTest {
-
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void addTest() {
+		MathOperations op=new MathOperations();
+		assertEquals(5,op.add(2,3));
+	}
+
+		@Test
+	void subTest() {
+		MathOperations op=new MathOperations();
+		assertEquals(-1,op.sub(2,3));
+	}
+
+		@Test
+	void mulTest() {
+		MathOperations op=new MathOperations();
+		assertEquals(6,op.mul(2,3));
+	}
+
+		@Test
+	void divTest() {
+		MathOperations op=new MathOperations();
+		assertEquals(0,op.div(2,3));
 	}
 
 }
