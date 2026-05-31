d65d2b7b33728ed29e35ffb8d5e1936d9e55bc24
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-06-01 03:02:15 +0530

    feat:[leela sai kumar meda] storing stream as alist using collect

--- day24/StreamAPI.java
+++ day24/StreamAPI.java
@@ -2,11 +2,13 @@ package src.day24;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamAPI{
     public static void main(String[] args) {
        List<Integer> list=Arrays.asList(1,2,3,4,5,6,7,8,9,10);
        list.stream().forEach(System.out::println); 
-       list.stream().map(n->n*1.0).forEach(System.out::println); 
+    List<Double> list2=list.stream().map(n->n*1.0).collect(Collectors.toList()); 
+    list2.forEach(System.out::println);
     }
 }
