5f1ad7faaacb9648f353c30e7c78d3b13af07102
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-06-02 08:46:53 +0530

    feat:[leela sai kumar meda] finding an even number is there or all are even numbers

--- day24/StreamAPI.java
+++ day24/StreamAPI.java
@@ -5,24 +5,28 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public class StreamAPI{
-    public static void main(String[] args) {
-       List<Integer> list=Arrays.asList(1,2,3,4,5,6,7,8,9,10);
-       list.stream().forEach(System.out::println); 
-    List<Double> list2=list.stream().map(n->n*1.0).collect(Collectors.toList()); 
+public class StreamAPI {
+  public static void main(String[] args) {
+    List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    list.stream().forEach(System.out::println);
+    List<Double> list2 = list.stream().map(n -> n * 1.0).collect(Collectors.toList());
     list2.forEach(System.out::println);
-    list.stream().filter(n->n%2==0).forEach(System.out::println);
+    list.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
     System.out.println("First even number in the stream");
-      Optional<Integer> opt=list.stream().filter(n->n%2==0).findFirst();
+    Optional<Integer> opt = list.stream().filter(n -> n % 2 == 0).findFirst();
     System.out.println(opt.get());
-    Optional<Integer> min=list.stream().min((a,b)->a.compareTo(b));
-  System.out.println("The min value in the stream is "+min.get());
-  Optional<Integer> max=list.stream().max((a,b)->a.compareTo(b));
-  System.out.println("The max value in the stream is "+max.get());
-  Optional<Integer> sum=list.stream().reduce((a,b)->a+b);
-  long size=list.stream().count();
-  System.out.println("The sum of stream is "+sum.get());
-  System.out.println("The avg of stream is "+sum.get()/size);
-  list.sorted().forEach(system.out::println);
-      }
+    Optional<Integer> min = list.stream().min((a, b) -> a.compareTo(b));
+    System.out.println("The min value in the stream is " + min.get());
+    Optional<Integer> max = list.stream().max((a, b) -> a.compareTo(b));
+    System.out.println("The max value in the stream is " + max.get());
+    Optional<Integer> sum = list.stream().reduce((a, b) -> a + b);
+    long size = list.stream().count();
+    System.out.println("The sum of stream is " + sum.get());
+    System.out.println("The avg of stream is " + sum.get() / size);
+    list.sorted().forEach(system.out::println);
+    List<Integer> numbers = Arrays.asList(1, 3, 5, 8);
+    boolean result = numbers.stream()
+        .anyMatch(n -> n % 2 == 0);
+    System.out.println(result);
+  }
 }
